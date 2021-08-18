72d48812180242ba882f01f6c6f004441a09c171
Fábio Aparecido da Silva Raminelli <phabioas@yahoo.com.br>
2021-08-17 22:23:39 -0300

    Melhoria

--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
 import 'package:shop/providers/products_provider.dart';
-import 'package:shop/widgets/product_item.dart';
+import 'package:shop/widgets/product_grid_item.dart';
 
 class ProductGrid extends StatelessWidget {
   final bool showFavoriteOnly;
